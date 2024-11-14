639c0b86cb51ac2407e572c67a46a7d0e8e55712
KageCL <cxlowery@email.sc.edu>
2024-11-13 20:58:37 -0500

    Added Contact Form

---
+++ impressions/src/components/ContactForm.js
@@ -0,0 +1,57 @@
+export default function Contact() {
+    const [result, setResult] = React.useState("");
+  
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "267c8075-2a5a-4196-818b-44779ab8ab56");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+  
+    return (
+      <div>
+        
+        <h1>Contact Form</h1>
+        <h2>Include questions such as shipping, pricing and event questions.</h2>
+        <form onSubmit={onSubmit}>
+
+            <p>
+                <label for="name">Name:</label>
+                <input type="text" name="name" required/>
+            </p>
+
+            <p>
+                <label for="email">Email:</label>
+                <input type="email" name="email" required/>
+            </p>
+
+            <p>
+                <label for="message">Message:</label>
+                <textarea name="message" required></textarea>
+            </p>
+
+          <button type="submit">Submit Form</button>
+  
+        </form>
+        <span>{result}</span>
+  
+      </div>
+    );
+  }
+  
--- impressions/src/pages/Contact.js
+++ impressions/src/pages/Contact.js
@@ -15,36 +15,6 @@ const Contact = () => {
                 <h2>Deliver is possible for locations in or around Sumter. Appointments must me made.</h2>
                 <h2>Deilver fees included.</h2>
 
-                <h1>Contact Form</h1>
-                <h2 id="form-desc">Include questions such as shipping, pricing and event questions.</h2>
-
-                <div id="form-area">
-                <form id="form-contact" action="https://api.web3forms.com/submit" method="POST" class="columns">
-
-<input type="hidden" name="access_key" value="267c8075-2a5a-4196-818b-44779ab8ab56"/>
-
-<p>
-<label for="name">Name:</label>
-<input type="text" name="name" required/>
-</p>
-<p>
-<label for="email">Email:</label>
-<input type="email" name="email" required/>
-</p>
-<p>
-<label for="message">Message:</label>
-<textarea name="message" rows="4" cols="50" required></textarea>
-</p>
-<p>
-<input type="hidden" name="redirect" value="https://web3forms.com/success"/>
-</p>
-<button type="submit">Submit Form</button>
-
-<p id="result"></p>
-</form>
-
-        </div>
-
            </div>
            <div id="social-media">
                 <h1>Social Media</h1>
